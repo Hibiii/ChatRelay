925717c28b8e013a0111fa0057eb9de90d384ead
Hibiii <69405847+Hibiii@users.noreply.github.com>
2020-12-05 19:32:05 -0300

    Add some scaffolding

--- chatrelay_http.js
+++ chatrelay_http.js
@@ -1,7 +1,25 @@
 var http = require("http")
+var url = require("url")
 
 http.createServer(function (request, response) {
-	response.writeHead(501)
-	response.write("501 Not Implemented :(")
-	response.end()
+	var requestPath = url.parse(request.url).pathname
+	if (request.method == "POST" && requestPath == "/messages") {
+		response.writeHead(501).end()
+		return
+	}
+	if (request.method != "GET") {
+		response.writeHead(405).end()
+		return
+	}
+	switch (requestPath) {
+		case "/status":
+			response.writeHead(501).end()
+			return
+		case "/defibrillators":
+			response.writeHead(501).end()
+			return
+		default:
+			response.writeHead(404).write("404: The endpoint \"" + requestPath + "\" does not exist.").end()
+			return
+	}
 }).listen(25566)
