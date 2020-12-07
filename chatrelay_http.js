795bb9e192aafd0a696c070f48dbbcf9e759ec07
Hibiii <69405847+Hibiii@users.noreply.github.com>
2020-12-07 20:18:48 -0300

    I don't think I need separate files for now

--- chatrelay_http.js
+++ chatrelay_http.js
@@ -1,3 +1,38 @@
+const mc = require('minecraft-protocol')
+var client = mc.createClient({
+	host: "localhost",   // optional
+	port: 25565,         // optional
+	username: "steve@minecraft.net",
+	password: "i love diamonds"
+})
+
+client.on('chat', function (packet) {
+	buffer.add(packet.message)
+});
+
+
+var buffer = []
+buffer.i = 0
+buffer.size = 32
+
+buffer.add = function (message) {
+	buffer.i %= buffer.size
+	buffer[buffer.i] = { data: message, time: Date.now() }
+	buffer.i++
+}
+
+buffer.getMessages = function (timestamp) {
+	let unreadMessages = []
+	let i = buffer.i;
+	do {
+		if (buffer[i] && buffer[i].time && buffer[i].time > timestamp)
+			unreadMessages.push(buffer[i])
+		i = (i + 1) % buffer.size 
+	} while (i != buffer.i)
+	return unreadMessages;
+}
+
+
 var http = require("http")
 var url = require("url")
 
@@ -11,13 +46,21 @@ http.createServer(function (request, response) {
 			try { timestamp = JSON.parse(data).from }
 			catch (e) { timestamp = -1 }
 		})
-		if (timestamp < 0)
-			return response.writeHead(400).end()
+		if (timestamp < 0) {
+			response.writeHead(400)
+			response.end()
+			return
+		}
 		let unreadMessages = buffer.getMessages(timestamp)
 		if (unreadMessages) {
-			return response.writeHead(200).write(unreadMessages).end()
+			response.writeHead(200)
+			response.write(unreadMessages)
+			response.end()
+			return
 		} else {
-			return response.writeHead(304).end()
+			response.writeHead(304)
+			response.end()
+			return
 		}
 		response.writeHead(501).end()
 		return
@@ -34,7 +77,9 @@ http.createServer(function (request, response) {
 			response.writeHead(501).end()
 			return
 		default:
-			response.writeHead(404).write("404: The endpoint \"" + requestPath + "\" does not exist.").end()
+			response.writeHead(404)
+			response.write("404: The endpoint \"" + requestPath + "\" does not exist.")
+			response.end()
 			return
 	}
 }).listen(25566)
